e4e0582735b2b755a1a528af6eaa2ea782dcdb26
any1ok <any3ok@naver.com>
2021-10-03 21:35:06 +0900

    추석트래픽 17676

--- [1차] 추석트래픽 17676/구현.cpp
+++ [1차] 추석트래픽 17676/구현.cpp
@@ -26,63 +26,72 @@
 
 using namespace std;
 
-int jisuf(int n) { // 자리수 확인하는 함수
-    int k = 0;
-    while (n != 0)
-    {
-        n = n / 10;
-        ++k;
-    }
-    return k;
+vector<string> Split(string str, char delim) {
+    vector<string> ret;
+    stringstream ss(str);
+    string temp;
 
+    while (getline(ss, temp, delim)) {
+        ret.push_back(temp);
+    }
+    return ret;
 }
 
 
-int solution(string s) {
+int solution(vector<string> lines) {
+    vector<string> temp; // 임시 저장소
+    vector<int> start_point; // 완료 시간
+    vector<int> end_point; // 시작 시간
+
+    int size = lines.size();
+    int start_p;
+    int end_p;
+    int doing;
+    for (int i = 0; i < size; i++) {
+        temp = Split(lines[i], ' ');
+        end_p = stoi(Split(temp[1], ':')[0]) * 3600000 + stoi(Split(temp[1], ':')[1]) * 60000 + stof(Split(temp[1], ':')[2]) * 1000;
+
+        end_point.push_back(end_p);
+        doing = stof(temp[2]) * 1000 - 1; // 진행시간
+        start_point.push_back(end_p - doing);
+
+    }
+    int check;
     int answer = 0;
-    int len = s.length();
-    vector<int> v1(1001, 100001);
-    if (len == 1) {
-        return 1;
-    }
-    for (int i = 1; i < len / 2 + 1; i++) { //  i == 중복 체크할 문자열 길이
-        int pull = len;
-        string tmp = "1";
-        int cnt = 1; // cnt == 중복된 개수 aa 일시 2개라서 1부터시작함
-        int k = 1;
-        int jisu = 0; // jisu == 자리값 - xxxxxxxxxxxy 10xy 의 경우 4자리라 체크해야됭
-        for (int j = 0; j < len; j += i) {
+    for (int i = 0; i < size; i++) {
+        check = 0;
 
-            int length = 0;
-            if (tmp == s.substr(j, i)) { // 중복된다면
-                pull = pull - i;
+        // 범위는 시작시점의 1초 전이다.
+        int range = start_point[i] - 999;
+        for (int i = 0; i < size; ++i)
+        {
+            // --------------------------------------------------------- 시작 시점 검사
 
-                cnt++;
-                if (jisuf(cnt) != jisu) {  // 자리수 올라가면 +1 해주는 로직
-                    jisu++;
-                    pull++;
-                }
+            // 저장소 초기화 && 본인추가
+            check = 0;
+
+            // 범위는 시작시점의 1초 전이다.
+            int range = start_point[i] - 999;
+
+            for (int j = 0; j < size; ++j)
+            {
+                // 1. 시작포인트가 범위에 포함하는 경우 종료시점은 무관하다.) 시작지점이 범위보다 크거나 같고 현재 시작지점보다 작을 경우 
+                // 2. 시작포인트가 범위 이전에 있지만 종료시점이 범위 이상에 있는 경우 ) 
+                if (range <= start_point[j] && start_point[j] <= start_point[i])
+                    check++;
+                else if (range >= start_point[j] && end_point[j] >= range)
+                    check++;
             }
-            else {
-                cnt = 1;
-                jisu = 0;
-            }
-            tmp = s.substr(j, i);
+
+            // 최대 요청량 갱신
+            if (answer < check)
+                answer = check;
         }
-        v1[i] = pull;
+
+        return answer;
+
+
+
     }
-    return *min_element(v1.begin() + 1, v1.end()); // 최솟값값
-
 }
-int main() {
-    string a;
 
-
-    while (1) {
-        cin >> a;
-
-        cout << solution(a) << endl;;
-    }
-
-
-}
